c8c646a96186ee7526b514dc8c52c1fe04a653e6
Brent Jones <bjones@bjones.net>
2019-11-08 18:10:45 -0600

    style tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -208,44 +208,44 @@ function createMap() {
         interactive: false
     });
 
-    // map.on('load', function() {
-    //     // Insert the layer beneath any symbol layer.
-    //     var layers = map.getStyle().layers;
+    map.on('load', function() {
+        // Insert the layer beneath any symbol layer.
+        var layers = map.getStyle().layers;
 
-    //     var labelLayerId;
-    //     for (var i = 0; i < layers.length; i++) {
-    //     if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-    //     labelLayerId = layers[i].id;
-    //     break;
-    //     }
-    //     }
+        var labelLayerId;
+        for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+        labelLayerId = layers[i].id;
+        break;
+        }
+        }
 
-    //     map.addLayer({
-    //     'id': '3d-buildings',
-    //     'source': 'composite',
-    //     'source-layer': 'building',
-    //     'filter': ['==', 'extrude', 'true'],
-    //     'type': 'fill-extrusion',
-    //     'minzoom': 15,
-    //     'paint': {
-    //     'fill-extrusion-color': '#aaa',
+        map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 11,
+        'paint': {
+        'fill-extrusion-color': '#ccc',
 
-    //     // use an 'interpolate' expression to add a smooth transition effect to the
-    //     // buildings as the user zooms in
-    //     'fill-extrusion-height': [
-    //     "interpolate", ["linear"], ["zoom"],
-    //     15, 0,
-    //     15.05, ["get", "height"]
-    //     ],
-    //     'fill-extrusion-base': [
-    //     "interpolate", ["linear"], ["zoom"],
-    //     15, 0,
-    //     15.05, ["get", "min_height"]
-    //     ],
-    //     'fill-extrusion-opacity': .6
-    //     }
-    //     }, labelLayerId);
-    //     });
+        // use an 'interpolate' expression to add a smooth transition effect to the
+        // buildings as the user zooms in
+        'fill-extrusion-height': [
+        "interpolate", ["linear"], ["zoom"],
+        15, 0,
+        15.05, ["get", "height"]
+        ],
+        'fill-extrusion-base': [
+        "interpolate", ["linear"], ["zoom"],
+        15, 0,
+        15.05, ["get", "min_height"]
+        ],
+        'fill-extrusion-opacity': .6
+        }
+        }, labelLayerId);
+        });
 
     map.on('load', function () {
 
